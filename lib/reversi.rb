e471df4734240f28ad6038f144a85f92f0ce74c9
David+Luke <david+luke@appacademy.io>
2013-03-26 11:04:17 -0700

    added flipping, fixed valid_move?

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -8,12 +8,13 @@ class Board
   end
 
   def valid_move?(move, player_color)
+  #  p 'off'
     return false if off_board?(move)
-
+  #  p 'occ'
     return false if occupied?(move)
-
+ #   p 'flip'
     return false if no_flip?(move, player_color)
-
+ #   p 'off'
     true
   end
 
@@ -26,27 +27,53 @@ class Board
   end
 
   def no_flip?(move, player_color)
+    pieces_to_flip(move, player_color).empty?
+  end
+
+  def pieces_to_flip(move, player_color)
     surrounding_spaces = [-1,0,1].product([-1,0,1])
     surrounding_spaces.delete_if {|x| x == [0,0]}
+    pieces_to_flip = []
 
     surrounding_spaces.each do |vec|
       space = @pos[vec[0] + move[0]][vec[1] + move[1]]
       next if space == :empty || space.nil?
-      next if space.color == player_color
-      i = 3
+      next if space == player_color
+      i = 2
+      direction_spaces = [[vec[0] + move[0],vec[1] + move[1]]]
       until space == :empty || off_board?([vec[0]*i + move[0],vec[1]*i + move[1]])
-        return true if space.color == player_color
+        if space == player_color
+          pieces_to_flip += direction_spaces
+          break
+        end
+        direction_spaces << [vec[0]*i + move[0],vec[1]*i + move[1]]
         space = @pos[vec[0]*i + move[0]][vec[1]*i + move[1]]
         i += 1
       end
     end
-    false
+    pieces_to_flip
+  end
+
+  def make_move(move, player_color)
+    if valid_move?(move, player_color)
+      @pos[move[0]][move[1]] = player_color
+      pieces_to_flip(move, player_color).each do |y, x|
+        @pos[y][x] = player_color == :black ? :black : :white
+      end
+    else
+      raise "Invalid move."
+    end
   end
 
 
 end
 
 class Piece
+  attr_accessor :color
+
+  def initialize(color)
+    @color = color
+  end
 end
 
 class Game
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -3,23 +3,17 @@ require 'reversi.rb'
 
 describe Piece do
   subject(:piece) {Piece.new}
-
-  describe '#swap_color' do
-
-  end
 end
 
 describe Board do
   subject(:board) {Board.new}
-  let(:black_piece) {double(:piece, :color => :black)}
-  let(:white_piece) {double(:piece, :color => :white)}
 
   # it "Should have four pieces in the middle" do
-  #   board.pos[3][3].color.should == :black
-  #   board.pos[3][3].should == white_piece
-  #   board.pos[4][4].should == white_piece
-  #   board.pos[3][4].should == black_piece
-  #   board.pos[4][3].should == black_piece
+  #   board.pos[3][3].should == :black
+  #   board.pos[3][3].should == :white
+  #   board.pos[4][4].should == :white
+  #   board.pos[3][4].should == :black
+  #   board.pos[4][3].should == :black
   # end
 
   describe '#valid_move?' do
@@ -30,29 +24,70 @@ describe Board do
     it "the move is on the board" do
       board.valid_move?(off_board_move, :black).should be_false
     end
+
     it "the move location is empty" do
-      board.pos[3][3] = black_piece
+      board.pos[3][3] = :black
       board.valid_move?(occupied_move, :black).should be_false
     end
+
     it "the move flips an opponent's piece" do
-      board.pos[2][3] = white_piece
-      board.pos[1][3] = black_piece
-      board.pos[2][1] = black_piece
+      board.pos[2][3] = :white
+      board.pos[1][3] = :black
+      board.pos[2][1] = :black
       board.valid_move?(doesnt_flip_move, :black).should be_false
+    end
+
+    it "Accepts a valid move" do
+      board.pos[2][3] = :black
+      board.pos[3][3] = :white
+      board.pos[4][3] = :white
+      board.valid_move?([5,3], :black).should be_true
     end
   end
 
+#  describe '#flip_piece' do
+#
+#  end
+
   describe '#make_move' do
-    it "places a piece" do
+    before(:each) do
+      board.pos[3][3] = :white
+      board.pos[4][3] = :white
+      board.pos[3][4] = :white
+      board.pos[4][4] = :white
+    end
+
+    describe "flipping pieces" do
+
+      it 'flips pieces vertically' do
+        board.pos[2][3] = :black
+        board.make_move([5,3], :black)
+        board.pos[3][3].should == :black && board.pos[4][3].should == :black
+      end
+
+      it 'flips pieces horizontally' do
+        board.pos[4][2] = :black
+        board.make_move([4,5], :black)
+        board.pos[4][3].should == :black && board.pos[4][4].should == :black
+      end
+
+      it 'flips pieces diagonally' do
+        board.pos[2][2] = :black
+        board.make_move([5,5], :black)
+        board.pos[3][3].should == :black && board.pos[4][4].should == :black
+      end
 
     end
+  end
+
+  describe '#win?' do
+
   end
 end
 
 describe Game do
 
   describe '#make_board' do
-
   end
 end
 
